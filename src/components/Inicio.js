5e8041566e9a5c7113c1984f67af269339e413f7
ElisonMartins <martinselison10@gmail.com>
2022-08-07 14:12:27 -0300

    add gif

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from './button.js';
 import Wave from './onda.js';
 import './Inicio.css';
+import Bg from "./img/aud.gif";
 
 export default ({})=>{
     return(
@@ -11,8 +12,9 @@ export default ({})=>{
                     <p>PomoCode</p>
                     <p><a href="#sobre-pomo" className='sobre-pomodoro'>Sobre</a></p> 
                 </header>
-                
                 <div className='container'>
+
+                <img className="gif" src={Bg} width="250" alt="404 image"/>
                     <h1 className='timer'>25:00</h1>
                     <Button/>
                 </div> 
