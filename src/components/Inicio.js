aaaf7699fdbd756f69b6589d46f62cba501bc111
Elison Martins <68920728+ElisonMartins@users.noreply.github.com>
2022-09-21 20:49:11 -0300

    Update Inicio.js

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Button from './button.js';
 import Wave from './onda.js';
 import './Inicio.css';
-import Bg from "./img/aud.gif";
 
 export default ({})=>{
     return(
@@ -13,15 +12,11 @@ export default ({})=>{
                     <p><a href="#sobre-pomo" className='sobre-pomodoro'>Sobre</a></p> 
                 </header>
                 <div className='container'>
-
-                <img className="gif" src={Bg} width="250" alt="404 image"/>
                     <h1 className='timer'>25:00</h1>
                     <Button/>
                 </div> 
                 <Wave/>
             </div>
-            
-            
         </>
     )
 }
